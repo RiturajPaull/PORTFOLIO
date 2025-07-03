c2cc4bdddec8001bc5202333060718e9279d513a
Rituraj Paul <ronnieshro@gmail.com>
2025-07-03 13:23:22 +0530

    fix: update baseURL to point to the deployed backend service

--- frontend/src/API/SummaryAPI.js
+++ frontend/src/API/SummaryAPI.js
@@ -1,4 +1,5 @@
-export const baseURL = "http://localhost:7000";
+export const baseURL =
+  "https://portfolio-backend-git-main-riturajpaulls-projects.vercel.app";
 export const SummaryAPI = {
   email: {
     url: "/api/send/email",
